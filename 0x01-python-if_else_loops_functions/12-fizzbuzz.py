64ef6a42de2a5bc7e05e60b288b97047a4c29024
Peter Mwai <petermwai549@gmail.com>
2022-11-29 07:19:14 -0800

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-
-fizzbuzz()
-print("")
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print("{:d} ".format(i), end="")
